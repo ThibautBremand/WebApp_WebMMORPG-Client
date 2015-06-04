bba5d67c0504c4a9a133e5e5c5dfea233e70723f
Thibaut Bremand <thibaut.bremand@orange.fr>
2015-06-04 23:04:06 +0200

    JS classes more object oriented, prototype removed. English translation added in the code. Map 100% converted into OO JS class

--- js/classes/Tileset.js
+++ js/classes/Tileset.js
@@ -1,27 +1,28 @@
 var tileSize = 16;
 
+// Titleset object - Methods & attributes
 function Tileset(url) {
-	// Chargement de l'image dans l'attribut image
+	// Loads the image in the image attribute
 	this.image = new Image();
-	this.image.referenceDuTileset = this;
+	this.image.tilesetReference = this;
 	this.image.onload = function() {
 		if(!this.complete) 
 			throw new Error("Erreur de chargement du tileset nommé \"" + url + "\".");
 		
 		// Largeur du tileset en tiles
-		this.referenceDuTileset.largeur = this.width / tileSize;
+		this.tilesetReference.largeur = this.width / tileSize;
 	}
 	this.image.src = "tilesets/" + url;
-}
 
-// Méthode de dessin du tile numéro "numero" dans le contexte 2D "context" aux coordonnées xDestination et yDestination
-Tileset.prototype.dessinerTile = function(numero, context, xDestination, yDestination) {
-	var xSourceEnTiles = numero % this.largeur;
-	if(xSourceEnTiles == 0) xSourceEnTiles = this.largeur;
-	var ySourceEnTiles = Math.ceil(numero / this.largeur);
-	
-	var xSource = (xSourceEnTiles - 1) * tileSize;
-	var ySource = (ySourceEnTiles - 1) * tileSize;
-	
-	context.drawImage(this.image, xSource, ySource, tileSize, tileSize, xDestination, yDestination, tileSize, tileSize);
+	// Draws the title number 'nb' in the 2D Context 'context' with the coordinates below
+	this.drawTitle = function (nb, context, xDestination, yDestination) {
+		var xSourceEnTiles = nb % this.largeur;
+		if(xSourceEnTiles == 0) xSourceEnTiles = this.largeur;
+		var ySourceEnTiles = Math.ceil(nb / this.largeur);
+		
+		var xSource = (xSourceEnTiles - 1) * tileSize;
+		var ySource = (ySourceEnTiles - 1) * tileSize;
+		
+		context.drawImage(this.image, xSource, ySource, tileSize, tileSize, xDestination, yDestination, tileSize, tileSize);
+	};
 }
--- js/rpg.js
+++ js/rpg.js
@@ -2,9 +2,10 @@ var tileSize = 16;
 
 var mapsToDraw = new Array();
 mapsToDraw.push("sans-titre");
-mapsToDraw.push("sans-titre2");
+//mapsToDraw.push("sans-titre2");
 
 var map = new Map(mapsToDraw);
+map.loadLayers();
 
 var joueur = new Personnage("exemple.png", 7, 14, DIRECTION.BAS);
 map.addPersonnage(joueur);
@@ -13,31 +14,31 @@ window.onload = function() {
 	var canvas = document.getElementById('canvas');
 	var ctx = canvas.getContext('2d');
 	
-	canvas.width  = map.getLargeur() * tileSize;
-	canvas.height = map.getHauteur() * tileSize;
+	canvas.width  = map.width * tileSize;
+	canvas.height = map.height * tileSize;
 	
 	setInterval(function() {
-		map.dessinerMap(ctx);
+		map.drawMap(ctx);
 	}, 40);
 	
-	// Gestion du clavier
+	// Keyboard management
 	window.onkeydown = function(event) {
-		// On récupère le code de la touche
+		// Retrieves the key code
 		var e = event || window.event;
 		var key = e.which || e.keyCode;
 		
 		switch(key) {
-			case 38 : case 122 : case 119 : case 90 : case 87 : // Flèche haut, z, w, Z, W
-				joueur.deplacer(DIRECTION.HAUT, map);
+			case 38 : case 122 : case 119 : case 90 : case 87 : // Up arrow, z, w, Z, W
+				joueur.move(DIRECTION.HAUT, map);
 				break;
-			case 40 : case 115 : case 83 : // Flèche bas, s, S
-				joueur.deplacer(DIRECTION.BAS, map);
+			case 40 : case 115 : case 83 : // Down arrow, s, S
+				joueur.move(DIRECTION.BAS, map);
 				break;
-			case 37 : case 113 : case 97 : case 81 : case 65 : // Flèche gauche, q, a, Q, A
-				joueur.deplacer(DIRECTION.GAUCHE, map);
+			case 37 : case 113 : case 97 : case 81 : case 65 : // Left arrow, q, a, Q, A
+				joueur.move(DIRECTION.GAUCHE, map);
 				break;
-			case 39 : case 100 : case 68 : // Flèche droite, d, D
-				joueur.deplacer(DIRECTION.DROITE, map);
+			case 39 : case 100 : case 68 : // Right arrow, d, D
+				joueur.move(DIRECTION.DROITE, map);
 				break;
 			default : 
 				//alert(key);
