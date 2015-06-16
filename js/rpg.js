450a6ff2bcc2fd0d147684533ea097d1f832fa9d
ThibautBremand <thibaut.bremand@orange.fr>
2015-06-17 00:19:58 +0200

    sends nickname to server, decodes action from server

--- js/rpg.js
+++ js/rpg.js
@@ -30,15 +30,19 @@ window.onload = function() {
 		switch(key) {
 			case 38 : case 122 : case 119 : case 90 : case 87 : // Up arrow, z, w, Z, W
 				joueur.move(DIRECTION.HAUT, map);
+				conn.send("MOVE:" + DIRECTION.HAUT + ":" + nickname);
 				break;
 			case 40 : case 115 : case 83 : // Down arrow, s, S
 				joueur.move(DIRECTION.BAS, map);
+				conn.send("MOVE:" + DIRECTION.BAS + ":" + nickname);
 				break;
 			case 37 : case 113 : case 97 : case 81 : case 65 : // Left arrow, q, a, Q, A
 				joueur.move(DIRECTION.GAUCHE, map);
+				conn.send("MOVE:" + DIRECTION.GAUCHE + ":" + nickname);
 				break;
 			case 39 : case 100 : case 68 : // Right arrow, d, D
 				joueur.move(DIRECTION.DROITE, map);
+				conn.send("MOVE:" + DIRECTION.DROITE + ":" + nickname);
 				break;
 			default : 
 				//alert(key);
--- js/serverConnection.js
+++ js/serverConnection.js
@@ -1,18 +1,40 @@
-var conn = new WebSocket('ws://localhost:8080');
+function message(user, action, param){
+	this.user = user;
+	this.action = action;
+	this.param = param;
+
+	this.toString = function() {
+		return this.user + ":" + this.action + ":" + this.param;
+	}
+}
+
+var nickname = prompt('Enter your nickname','guest');
+
+var conn = new WebSocket('ws://localhost:8080' + '?' + nickname);
 
 conn.onopen = function(e) {
 	alert ("Connection established!");
-    console.log("Connection established!");
+    //console.log("Connection established!");
 };
 
 conn.onmessage = function(e) {
-    console.log(e.data);
+    var mess = e.data.split(":");
+    switch(mess[0]) {
+    case "ENTER":
+        alert (e.data);
+        break;
+    case "MOVE":
+    	
+    	break;
+    default:
+        break;
+	} 
 };
 
 function keyTreatment(evenement){
     //on a accès ici au code de la touche du clavier 
     //via la variable evenement.which
-    conn.send(evenement.which);
+    //conn.send(evenement.which);
 }
 $(function(){
     $(document).keydown(keyTreatment);
